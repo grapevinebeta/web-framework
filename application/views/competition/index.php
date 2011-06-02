82e95f7ae46d909a68f5c40374c437c799744a20
Tomasz Jaskowski <tomasz.jaskowski@polcode.com>
2011-06-02 18:41:53 +0200

    added competition controller and view

---
+++ application/views/competition/index.php
@@ -0,0 +1,83 @@
+<div id="boxes-holder">
+
+    <div class="box-container">
+        <div class="box-container-right"></div>
+        <div class="box-container-left"></div>
+        <div class="clear"></div>
+    </div>
+
+    <div class="box-container active">
+        <div id="box-social-activity" class="box">
+            <div class="box-header">
+                <div class="box-header-right-buttons">
+                    <a class="box-header-button-show-graph" href="#" title="<?php echo __('Close'); ?>">g</a>
+                    <a class="box-header-button-dashboard-pin" href="#" title="<?php echo __('Close'); ?>">p</a>
+                </div>
+                <div class="box-header-left-buttons">
+                    <a class="box-header-button-move" href="#" title="<?php echo __('Move'); ?>">m</a>
+                </div>
+                <?php echo __('Competitors'); ?>
+            </div>
+            <div class="box-content padding-5"></div>
+        </div>
+        <div class="box-container-left">
+        </div>
+        <div class="box-container-right">
+        </div>
+        <div class="clear"></div>
+    </div>
+    
+    <div class="box-container">
+        <div class="box-container-right"></div>
+        <div class="box-container-left"></div>
+        <div class="clear"></div>
+    </div>
+
+    <div class="box-container active">
+        <div id="box-recent-reviews" class="box">
+            <div class="box-header">
+                <div class="box-header-right-buttons">
+                    <a class="box-header-button-show-graph" href="#" title="Close">g</a>
+                    <a class="box-header-button-dashboard-pin" href="#" title="Close">p</a>
+                </div>
+                <div class="box-header-left-buttons">
+                    <a class="box-header-button-move" href="#" title="Move">m</a>
+                </div>
+                <?php echo __('Recent Reviews'); ?>
+            </div>
+            <div class="box-content padding-5"></div>
+        </div>
+        <div class="clear"></div>
+    </div>
+    
+    <div class="box-container">
+        <div class="box-container-right"></div>
+        <div class="box-container-left"></div>
+        <div class="clear"></div>
+    </div>
+    
+    <div class="box-container active">
+        <div id="box-social-activity-details" class="box">
+            <div class="box-header">
+                <div class="box-header-right-buttons">
+                    <a class="box-header-button-show-graph" href="#" title="Close">g</a>
+                    <a class="box-header-button-dashboard-pin" href="#" title="Close">p</a>
+                </div>
+                <div class="box-header-left-buttons">
+                    <a class="box-header-button-move" href="#" title="Move">m</a>
+                </div>
+                <?php echo __('Social Activity Details'); ?>
+            </div>
+            <div class="box-content padding-5"></div>
+        </div>
+        <div class="clear"></div>
+    </div>
+    
+    <div class="box-container">
+        <div class="box-container-right"></div>
+        <div class="box-container-left"></div>
+        <div class="clear"></div>
+    </div>
+    
+</div>
+<div class="clear"></div>
