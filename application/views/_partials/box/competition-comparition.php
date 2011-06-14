0642a3e335baff737f5cd2e3dbbbf336e38b5b7c
Jacek Kromski <jacek.kromski@polcode.com>
2011-06-14 17:06:48 +0200

    new box file structure

---
+++ application/views/_partials/box/competition-comparition.php
@@ -0,0 +1,13 @@
+<div id="box-competition-comparision" class="box">
+    <?php echo View::factory(
+        '_partials/box/header', 
+        array(
+            'caption' => __('Competition comparision'),
+            'buttons' => array('dashboard-pin', 'move'),
+            )
+        ); 
+    ?>
+    <div class="box-content">
+        <div id="box-competition-comparision-graph-holder" class="graph-holder"></div>
+    </div>
+</div>
---
+++ application/views/_partials/box/ogsi.php
@@ -0,0 +1,10 @@
+<div id="box-ogsi" class="box">
+    <?php echo View::factory(
+        '_partials/box/header', 
+        array(
+            'caption' => __('Grapevine OGSI'),
+            )
+        ); 
+    ?>
+    <div class="box-content"></div>
+</div>
