32caf4eb726e87ca385ee2594d58aecc58f19057
Lukasz Adamczewski <lukasz.adamczewski@polcode.pl>
2011-06-27 18:33:41 +0200

    changes

---
+++ application/views/_partials/box/generic-row.php
@@ -0,0 +1,78 @@
+<tr class="expanded">
+    <td colspan="5">
+
+        <form action="">
+            <div class="details" style="display: none;">
+                <div style="clear: both;">
+                    <div class="recentReviewDetailsForm">
+                        <h2 class="review-details-title"></h2>
+                        <p class="review-details-review"></p>
+                        <form action="" method="post">
+                            <table class="wide" style="vertical-align: top;">
+                                <tr>
+                                    <td class="a-right">
+                                        <?php echo __('Categorize'); ?>:
+                                    </td>
+                                    <td class="a-left">
+                                        <select name="category">
+                                            <option value=""></option>
+                                            <option value="category 1">category 1</option>
+                                            <option value="category 2">category 2</option>
+                                        </select>
+                                    </td>
+                                    <td class="a-right">
+                                        <?php echo __('Keywords'); ?>:
+                                        <input type="text" value="" name="keywords" />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="a-right">
+                                        <?php echo __('Review Notes'); ?>:
+                                        <br /><br />
+                                        <span class="i">
+                                            (<?php echo __('Internal use only'); ?>)
+                                        </span>
+                                    </td>
+                                    <td colspan="2">
+                                        <textarea type="text" name="notes" style="width: 99%; height: 75px;"></textarea>
+                                    </td>
+                                </tr>
+                            </table>
+                        </form>
+                    </div>
+                    <div class="recentReviewDetailsMenu">
+                        <p></p>
+                        <div class="a-center v-padding-2">
+                            <span class="recent-review-status-icon open"></span>
+                            <p>
+                                (<?php echo __('After responding to a review check the completed box'); ?>)
+                            </p>
+                        </div>
+                        <div class="v-padding-2">
+                            <span class="mono-icon icon-checkbox"></span>
+                            <?php echo __('Completed'); ?>
+                        </div>
+                        <div class="v-padding-2">
+                            <span class="mono-icon icon-email"></span>
+                            Email
+                        </div>
+                        <div class="v-padding-2">
+                            <span class="mono-icon icon-todo"></span>
+                            <?php echo __('Flag "To Do"'); ?>
+                        </div>
+                        <div class="v-padding-2">
+                            <span class="mono-icon icon-goto"></span>
+                            <?php echo __('Go to Review'); ?>
+                        </div>
+                    </div>
+                </div>
+                <p class="recentReviewDetailsButtons a-right">
+                    <a class="reply-to-review-button" href=""></a>
+                    <a class="save-button" href=""></a>
+                </p>
+            </div>
+        </form>
+
+
+    </td>
+</tr>
