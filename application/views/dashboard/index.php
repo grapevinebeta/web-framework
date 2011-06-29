97dfa4888e823bf0c96153de2ecae72254afdafb
Lukasz Adamczewski <lukasz.adamczewski@polcode.pl>
2011-06-29 18:15:01 +0200

    changes

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -8,7 +8,7 @@
 
 <div id="boxes-holder">
 
-    <div class="box-container active">
+    <div class="box-container active ignore">
         <?php echo view::factory('_partials/box/reviews-inbox'); ?>
     </div>
 
