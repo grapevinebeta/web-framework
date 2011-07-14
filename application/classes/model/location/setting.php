4e9dd61adf17346cb1cf466e3ffdcfa34c25c7de
Tomasz Jaskowski <tomasz.jaskowski@polcode.com>
2011-07-14 17:39:42 +0200

    switched from pdo into mysql; added support for ORM find_all() method

---
+++ application/classes/model/location/setting.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * The class for a single setting.
+ * @todo Maybe make it extend ORM?
+ */
+class Model_Location_Setting extends ORM {
+
+}
--- application/classes/model/location/settings.php
+++ application/classes/model/location/settings.php
@@ -7,7 +7,21 @@
  */
 class Model_Location_Settings {
 
-    
+    /**
+     * Associative array with names of settings as keys and collections of
+     * settings errors as values
+     * @var array
+     */
+    private $_settings = array(
+        'competitor'            => null,
+        'newsletter'            => null,
+        'facebook_oauth_token'  => null,
+        'twitter_search'        => null,
+        'twitter_account'       => null,
+        'filter_search'         => null,
+        'gblog_search'          => null,
+        'youtube_search'        => null,
+    );
 
     public function  __construct($location_id) {
         
