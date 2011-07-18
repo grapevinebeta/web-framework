21440b386ddebd14da45cc586bb2ca2704ecc9a9
Tomasz Jaskowski <tomasz.jaskowski@polcode.com>
2011-07-18 18:17:28 +0200

    added validation rules for General Location Settings

--- application/classes/model/location.php
+++ application/classes/model/location.php
@@ -9,11 +9,46 @@ class Model_Location extends ORM {
 
     public function rules() {
         return array(
+            'location_name' => array(
+                array('max_length', array(':value',50)),
+            ),
+            'address1' => array(
+                array('max_length', array(':value',45)),
+            ),
+            'address2' => array(
+                array('max_length', array(':value',45)),
+            ),
+            'city' => array(
+                array('max_length', array(':value',45)),
+            ),
+            'state' => array(
+                array('max_length', array(':value',2)),
+            ),
+            'zip' => array(
+                array('max_length', array(':value',12)),
+            ),
+            'phone' => array(
+                array('max_length', array(':value',20)),
+            ),
+            'url' => array(
+                array('max_length', array(':value',255)),
+            ),
+            'owner_name' => array(
+                array('max_length', array(':value',45)),
+            ),
             'owner_email' => array(
                 array('email', null),
                 array('min_length', array(':value',6)),
-                array('not_empty', null),
+                array('max_length', array(':value',255)),
+                //array('not_empty', null),
             ),
+            'owner_phone' => array(
+                array('max_length', array(':value',20)),
+            ),
+            'owner_ext' => array(
+                array('max_length', array(':value',45)),
+            ),
+            // @todo add rule for billing type
         );
     }
     
