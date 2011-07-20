3af3829c706fba76ae3c42941c71fbf4e472f5e4
Tomasz Jaskowski <tomasz.jaskowski@polcode.com>
2011-07-20 17:09:39 +0200

    moved list of users into partial view

--- application/views/account/users.php
+++ application/views/account/users.php
@@ -5,56 +5,16 @@
     <h2 class="content-section-title"><?php echo __('User Accounts'); ?>:</h2>
     <div id="account-users-list-section" class="padding-5">
         <div id="account-users-list-holder">
-            <form action="" method="post">
-            <table class="wide data-grid" cellpadding="5">
-                <tr>
-                    <th class="a-left">
-                        <?php echo __('User Accounts'); ?>
-                    </th>
-                    <th class="a-center">
-                        <?php echo __('Admin'); ?>
-                    </th>
-                    <th class="a-center">
-                        <?php echo __('Read Only'); ?>
-                    </th>
-                    <th class="a-center">
-                        <?php echo __('Delete'); ?>
-                    </th>
-                </tr>
-                <?php if (!empty($users)): ?>
-                <?php foreach ($users as $k => $user): ?>
-                <?php $_inputPrefix = 'users['. $user->id .']'; ?>
-                <tr class="<?php echo ($k % 2?'even':'odd'); ?>">
-                    <td>
-                        <?php echo form::hidden($_inputPrefix . '[id]', $user->id); ?>
-                        <?php echo $user->firstname.' '.$user->lastname, ' ', html::anchor('#', __('edit'), array(
-                            'data-action' => 'edit',
-                            'data-user-id' => $user->id,
-                        )); ?>
-                    </td>
-                    <td class="a-center">
-                        <?php echo form::radio($_inputPrefix . '[role]', 'admin', false); ?>
-                    </td>
-                    <td class="a-center">
-                        <?php echo form::radio($_inputPrefix . '[role]', 'user', true); ?>
-                    </td>
-                    <td class="a-center">
-                        <?php echo html::anchor(
-                            '#', 'x',
-                            array('class' => 'confirm-required',
-                                  'onclick' => "return confirm('". __('Are you sure?') ."')")) ?>
-                    </td>
-                </tr>
-                <?php endforeach; ?>
-                <?php endif; ?>
-            </table>
-            </form>
+            <?php echo View::factory('account/users/list', array(
+                'users' => $users,
+            )); ?>
         </div>
     </div>
     <h2 class="content-section-title"><?php echo __('Add / Edit User'); ?></h2>
     <div id="account-users-edit-section" class="padding-5">
         <div id="account-users-edit-holder">
             <form action="" method="post">
+                <input type="hidden" value="" name="id" />
                 <table>
                     <tr>
                         <td class="a-right">
---
+++ application/views/account/users/list.php
@@ -0,0 +1,44 @@
+<form action="" method="post">
+    <table class="wide data-grid" cellpadding="5">
+        <tr>
+            <th class="a-left">
+                <?php echo __('User Accounts'); ?>
+            </th>
+            <th class="a-center">
+                <?php echo __('Admin'); ?>
+            </th>
+            <th class="a-center">
+                <?php echo __('Read Only'); ?>
+            </th>
+            <th class="a-center">
+                <?php echo __('Delete'); ?>
+            </th>
+        </tr>
+        <?php if (!empty($users)): ?>
+            <?php foreach ($users as $k => $user): ?>
+                <?php $_inputPrefix = 'users['. $user->id .']'; ?>
+                <tr class="<?php echo ($k % 2?'even':'odd'); ?>">
+                    <td>
+                        <?php echo form::hidden($_inputPrefix . '[id]', $user->id); ?>
+                        <?php echo $user->firstname.' '.$user->lastname, ' ', html::anchor('#', __('edit'), array(
+                            'data-action' => 'edit',
+                            'data-user-id' => $user->id,
+                        )); ?>
+                    </td>
+                    <td class="a-center">
+                        <?php echo form::radio($_inputPrefix . '[role]', 'admin', false); ?>
+                    </td>
+                    <td class="a-center">
+                        <?php echo form::radio($_inputPrefix . '[role]', 'user', true); ?>
+                    </td>
+                    <td class="a-center">
+                        <?php echo html::anchor(
+                            '#', 'x',
+                            array('class' => 'confirm-required',
+                                  'onclick' => "return confirm('". __('Are you sure?') ."')")) ?>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        <?php endif; ?>
+    </table>
+</form>
