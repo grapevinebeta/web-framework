f8f78a9ce1ed42796705d6689cd043ef0e0b90f8
Tomasz Jaskowski <tomasz.jaskowski@polcode.com>
2011-07-22 17:07:24 +0200

    fixed encoding within the database - changed from Swedish to Unicode

---
+++ materials/sql/20110722_1653_enconding_fix.sql
@@ -0,0 +1,164 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.3
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Czas wygenerowania: 20 Lip 2011, 15:19
+-- Wersja serwera: 5.1.46
+-- Wersja PHP: 5.3.3
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+DROP TABLE IF EXISTS `alerts`;
+CREATE TABLE IF NOT EXISTS `alerts` (
+  `alert_id` int(11) NOT NULL AUTO_INCREMENT,
+  `location_id` int(11) NOT NULL,
+  `type` varchar(12) NOT NULL,
+  `criteria` text NOT NULL COMMENT 'only typed text to work with custom keyword entries\n',
+  `use_default` tinyint(1) NOT NULL,
+  PRIMARY KEY (`alert_id`),
+  KEY `fk_alerts_locations` (`location_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `alert_emails`;
+CREATE TABLE IF NOT EXISTS `alert_emails` (
+  `alert_id` int(11) NOT NULL,
+  `emails_id` int(11) NOT NULL,
+  PRIMARY KEY (`alert_id`,`emails_id`),
+  KEY `fk_alert_emails_alerts` (`alert_id`),
+  KEY `fk_alert_emails_emails` (`emails_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `company`;
+CREATE TABLE IF NOT EXISTS `company` (
+  `company_id` int(11) NOT NULL,
+  `name` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`company_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `company_locations`;
+CREATE TABLE IF NOT EXISTS `company_locations` (
+  `location_id` int(11) NOT NULL,
+  `company_id` int(11) NOT NULL,
+  PRIMARY KEY (`location_id`,`company_id`),
+  KEY `fk_locations_company` (`company_id`),
+  KEY `fk_locations` (`location_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `company_users`;
+CREATE TABLE IF NOT EXISTS `company_users` (
+  `company_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`company_id`,`user_id`),
+  KEY `fk_user` (`user_id`),
+  KEY `fk_comany` (`company_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `emails`;
+CREATE TABLE IF NOT EXISTS `emails` (
+  `email_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(200) DEFAULT NULL,
+  `location_id` int(11) NOT NULL,
+  PRIMARY KEY (`email_id`,`location_id`),
+  KEY `fk_emails_locations` (`location_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `locations`;
+CREATE TABLE IF NOT EXISTS `locations` (
+  `location_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `location_name` varchar(50) DEFAULT NULL,
+  `address1` varchar(45) DEFAULT NULL,
+  `address2` varchar(45) DEFAULT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  `state` varchar(2) DEFAULT NULL,
+  `zip` varchar(12) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `owner_name` varchar(45) DEFAULT NULL,
+  `owner_email` varchar(255) DEFAULT NULL,
+  `owner_phone` varchar(20) DEFAULT NULL,
+  `owner_ext` varchar(45) DEFAULT NULL,
+  `billing_type` enum('charge','invoice') DEFAULT NULL,
+  PRIMARY KEY (`location_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `location_settings`;
+CREATE TABLE IF NOT EXISTS `location_settings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(45) NOT NULL COMMENT 'competitor,newsletter,facebook_oauth_token,twitter_search,twitter_account,filter_search,gblog_search,youtube_search',
+  `value` varchar(225) DEFAULT NULL,
+  `location_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`location_id`),
+  KEY `fk_location_settings_locations` (`location_id`),
+  KEY `idx_setting_type` (`type`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `location_users`;
+CREATE TABLE IF NOT EXISTS `location_users` (
+  `location_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`location_id`,`user_id`),
+  KEY `fk_location_users_locations` (`location_id`),
+  KEY `fk_location_users_users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `newsletters`;
+CREATE TABLE IF NOT EXISTS `newsletters` (
+  `email_id` int(11) NOT NULL,
+  `location_id` int(11) NOT NULL,
+  PRIMARY KEY (`email_id`,`location_id`),
+  KEY `fk_newsletters_emails` (`email_id`,`location_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `roles_users`;
+CREATE TABLE IF NOT EXISTS `roles_users` (
+  `user_id` int(10) unsigned NOT NULL,
+  `role_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `fk_role_id` (`role_id`),
+  KEY `fk_user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `logins` int(10) NOT NULL DEFAULT '0',
+  `last_login` int(10) unsigned DEFAULT NULL,
+  `username` varchar(32) NOT NULL,
+  `email` varchar(127) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `firstname` varchar(45) DEFAULT NULL,
+  `lastname` varchar(45) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_username` (`username`),
+  UNIQUE KEY `uniq_email` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `user_tokens`;
+CREATE TABLE IF NOT EXISTS `user_tokens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `user_agent` varchar(40) NOT NULL,
+  `token` varchar(40) NOT NULL,
+  `type` varchar(100) NOT NULL,
+  `created` int(10) unsigned NOT NULL,
+  `expires` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_token` (`token`),
+  KEY `fk_user_tokens` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
