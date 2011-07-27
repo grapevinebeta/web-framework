0946f5d8d638b98baf09ff7f4d91e095457c1ed4
Tomasz Jaskowski <tomasz.jaskowski@polcode.com>
2011-07-27 15:42:44 +0200

    changed SQL import file to not colliding with already existing settings

--- materials/sql/20110726_23_49_settings_for_facebook.sql
+++ materials/sql/20110726_23_49_settings_for_facebook.sql
@@ -1,3 +1,3 @@
-INSERT INTO `location_settings` 
-VALUES  (1,'facebook_oauth_token','114877061941522|08f10f3aa1001609fc85ce56.1-100000269555595|225509304157435|CjMnXHrGLNOMdgkgcsAEw-1XZFk',1),
- (2,'facebook_page_id','225509304157435',1);
+INSERT INTO `location_settings` (`type`, `value`, `location_id`)
+VALUES  ('facebook_oauth_token','114877061941522|08f10f3aa1001609fc85ce56.1-100000269555595|225509304157435|CjMnXHrGLNOMdgkgcsAEw-1XZFk',1),
+ ('facebook_page_id','225509304157435',1);
