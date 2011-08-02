9e0ef35cf9db05fe7f0c2a17b0e6067c251e9471
Łukasz Adamczewski <tworzenieweb@gmail.com>
2011-08-02 12:44:31 +0200

    fix exporting

---
+++ application/views/resources/privacy.php
@@ -0,0 +1,24 @@
+<div id="boxes-holder">
+
+<h1>
+    Privacy
+
+</h1>
+
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat, felis non interdum interdum, velit massa ullamcorper lectus, sed tempor lorem magna placerat justo. Vestibulum et aliquet sem. Donec tellus lacus, luctus hendrerit facilisis id, eleifend id urna. Pellentesque eu scelerisque neque. Pellentesque sagittis, tortor iaculis tristique tristique, sapien turpis fermentum felis, nec molestie urna velit sit amet mi. Morbi feugiat tellus in purus scelerisque at aliquet odio tincidunt. Duis volutpat ipsum at turpis pulvinar pharetra aliquet velit pretium. Mauris condimentum nunc nec mi ultrices suscipit adipiscing purus auctor. Nunc in lorem in sem dignissim aliquet. Praesent gravida, leo et semper porta, arcu purus rutrum felis, in consequat quam elit ac libero.
+</p>
+<p>
+    Cras varius erat ac tortor laoreet quis lacinia quam luctus. Sed sed tortor in enim vehicula lobortis ac interdum neque. Morbi non nisl quam, non facilisis ligula. Mauris condimentum sapien non nunc egestas consequat sit amet non est. Aliquam ac velit metus, non faucibus enim. Suspendisse lectus quam, viverra eget dignissim sit amet, lobortis ac metus. Etiam interdum tempor nisl at ornare. Ut vitae velit tortor. Vestibulum tempus dolor ut urna tempus pharetra. Sed sem neque, iaculis id adipiscing non, adipiscing vitae risus.
+</p>
+<p>
+    Vestibulum eu felis eget eros lobortis rutrum a sit amet risus. Etiam nisi lorem, faucibus in porta quis, consequat at ipsum. Duis pulvinar pellentesque turpis, sit amet hendrerit dui auctor vel. Curabitur eu leo leo. Nunc lobortis pulvinar sodales. Morbi egestas, sem non vestibulum consectetur, urna arcu egestas sapien, quis varius est orci eu odio. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec tempor, turpis et gravida eleifend, lectus neque posuere est, quis vulputate est enim vitae purus. Donec fermentum condimentum condimentum. Cras accumsan tristique nisi a vehicula. Nullam tincidunt odio non nulla euismod gravida. Cras magna ligula, scelerisque a dapibus eu, placerat in purus. Nam eget massa libero. Aliquam erat volutpat. Cras justo tortor, scelerisque a pellentesque a, interdum eu elit. Pellentesque diam turpis, malesuada eget molestie vitae, hendrerit ac nisl.
+</p>
+<p>
+    In porttitor, est nec dictum volutpat, justo arcu tempus diam, eu pellentesque justo diam nec enim. Vivamus sed dui risus. Aenean ullamcorper elit sit amet neque tincidunt nec rutrum magna elementum. Curabitur luctus ante eget ante sollicitudin iaculis. Sed in erat nibh. Curabitur nulla felis, fringilla id bibendum ut, vestibulum in velit. Sed eu volutpat dolor. Nullam nibh quam, accumsan id congue quis, egestas sit amet sapien. Suspendisse potenti. Integer vel dolor justo. In consectetur laoreet justo id dignissim. Suspendisse vehicula est vitae nulla luctus lacinia. Quisque sed lectus urna. Sed venenatis ipsum eu elit hendrerit malesuada vehicula felis cursus.
+</p>
+<p>
+    Vestibulum in congue tortor. Praesent a bibendum neque. Proin lobortis sapien vitae nisi venenatis id tempus lectus lacinia. Quisque tempus elementum felis, eu mollis velit faucibus ut. In fermentum augue vel mauris commodo eget condimentum ante viverra. Aenean semper mollis elit quis viverra. Duis vitae tortor quis sem sollicitudin adipiscing. Morbi scelerisque nunc in dolor tincidunt sit amet suscipit est viverra. Maecenas hendrerit imperdiet justo, vitae lobortis est lobortis iaculis. Praesent aliquam diam ut arcu semper eleifend.
+</p>
+
+</div>
