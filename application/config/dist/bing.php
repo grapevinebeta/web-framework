62a1865635a2d3d2351441d11d87343ea7270f83
Keyston <keyston@conceptual-ideas.com>
2011-08-24 14:25:28 -0500

    added bing config

---
+++ application/config/dist/bing.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+    /**
+     * Created by JetBrains PhpStorm.
+     * User: Keyston
+     * Date: 7/30/11
+     * Time: 8:53 AM
+     */
+
+    return array(
+        'appId' => '0EA9C6DD66ECE899FE791E9CB4C5FADF2DB61D7E'
+    );
