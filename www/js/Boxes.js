d1dc1657c19744d71490a01db67be9b4fa67beba
Łukasz Adamczewski <tworzenieweb@gmail.com>
2011-09-13 12:15:31 +0200

    fix bugs

--- www/js/Boxes.js
+++ www/js/Boxes.js
@@ -1613,7 +1613,7 @@ var BC_Scoreboard = BoxController.extend({
 
                     ratio = (rating.neutral / maxValue) * 100;
 
-                    if(ratio > 20)
+                    if(ratio > 15)
                         bar.children('.bar-value').text(rating.neutral);
 
                     bar.css('width', ((rating.neutral + rating.positive)/total)*100+'%');
