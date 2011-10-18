15aa1120d3a8a3fbcc9aa0fe96363cc93803b08a
Łukasz Adamczewski <tworzenieweb@gmail.com>
2011-10-18 10:38:16 +0200

    change location widget

--- www/js/common.js
+++ www/js/common.js
@@ -25,7 +25,7 @@ var APP = {
                 opt += '<option value="' + key + '">' + value + '</option>';
             });
 
-            $('body').append('<div id="loc">Select location: <select name="loc">' + opt + '</select></div>');
+            $('body').append('<div id="loc">Select location:<br /> <select name="loc">' + opt + '</select></div>');
             $('#loc').bind('change', function() {
 
                 self.location.current_location_id =  parseInt($('option:selected',this).val());
